a083690192f222e3443381e8c507d570dcb4c3c7
Sanduni93 <nirodhajayaweera1993@gmail.com>
2018-08-01 23:52:57 +0530

    Another Pattern

---
+++ Tuto5Q2.java
@@ -0,0 +1,24 @@
+class Tuto5Q2{
+	
+	public static void main(String args[]){
+		
+		int i,j;
+		
+		for(i=1;i<10;i++){
+			
+			
+			
+			for(int k = 1;k<=i;k++){
+				
+				System.out.print(i);	
+				
+				System.out.print(" ");	
+			}
+			
+			
+				System.out.println();	
+		}
+	
+	
+	
+}}
